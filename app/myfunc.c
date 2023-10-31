7cf5d6dbf540bfe24e93a1c41cdb21d41c6c88f9
Samvel Kocharyan <kocharya@cs.petrsu.ru>
2023-10-31 10:48:36 +0300

    main

--- app/myfunc.c
+++ app/myfunc.c
@@ -33,18 +33,17 @@ int fibonachi(int num) {
     return next;
 }
 
-double* square(double a, double b, double c) {
+int square(double a, double b, double c, double* roots) {
     double discriminant = b * b - 4 * a * c;
-    double* roots = malloc(2 * sizeof(double));
 
     if (discriminant >= 0) {
         roots[0] = (-b + sqrt(discriminant)) / (2 * a);
         roots[1] = (-b - sqrt(discriminant)) / (2 * a);
     } else {
-        return NULL;
+        return 1;
     }
 
-    return roots;
+    return 0;
 }
 
 void printStdoutMessages() {
