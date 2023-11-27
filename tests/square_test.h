64078a35b684ab9c94abd2384e3fbd4be037e664
Samvel Kocharyan <kocharya@cs.petrsu.ru>
2023-11-27 23:13:27 +0300

    fix test

--- tests/square_test.h
+++ tests/square_test.h
@@ -9,9 +9,8 @@ extern "C" {
 
 TEST(SQUARETest, discr_greater_0) {
     double a = 1, b = -3, c = 2;
-    int numRoots = 2;
     double roots[2];
-    int test = square(a, b, c, roots);
+    square(a, b, c, roots);
     ASSERT_EQ(roots[0], 2.0);
     ASSERT_EQ(roots[1], 1.0);
 }
@@ -26,7 +25,7 @@ TEST(SQUARETest, discr_less_0) {
 TEST(SQUARETest, discr_eq_0) {
     double a = 1, b = -2, c = 1;
     double roots[2];
-    int test = square(a, b, c, roots);
+    square(a, b, c, roots);
     ASSERT_EQ(roots[0], 1.0);
     ASSERT_EQ(roots[1], 1.0);
 }
